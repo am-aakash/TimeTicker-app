62080c2a85610264c48f8e51bbf89fccf3d15ff4
am_aakash <akashchaudhary12121@gmail.com>
2021-08-10 18:10:57 +0530

    Ui implementation done

---
+++ lib/screens/timer_text.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_timer/bloc/timer_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TimerText extends StatelessWidget {
+  const TimerText({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final duration =
+        23; //context.select((TimerBloc bloc) => bloc.state.duration);
+    final minutesStr =
+        ((duration / 60) % 60).floor().toString().padLeft(2, '0');
+    final secondsStr = (duration % 60).floor().toString().padLeft(2, '0');
+    return Text(
+      '$minutesStr:$secondsStr',
+      style: Theme.of(context).textTheme.headline1,
+    );
+  }
+}
